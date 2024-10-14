f6dbda8312e80c76e165a11fe991f2de51da9c8f
ssolit <sol@seismic.systems>
2024-10-14 19:25:56 +0000

    clarified struct comments

--- src/tx_io/structs.rs
+++ src/tx_io/structs.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 
+// Struct for serializing the io encryption request
 #[derive(Debug, Serialize, Deserialize)]
 pub struct IoEncryptionRequest {
     pub msg_sender: secp256k1::PublicKey,
@@ -7,12 +8,13 @@ pub struct IoEncryptionRequest {
     pub nonce: u64,
 }
 
-// Struct for serializing the response
+// Struct for serializing the io encryption response
 #[derive(Debug, Serialize, Deserialize)]
 pub struct IoEncryptionResponse {
     pub encrypted_data: Vec<u8>,
 }
 
+// Struct for serializing the io decryption request
 #[derive(Debug, Serialize, Deserialize)]
 pub struct IoDecryptionRequest {
     pub msg_sender: secp256k1::PublicKey,
@@ -20,7 +22,7 @@ pub struct IoDecryptionRequest {
     pub nonce: u64,
 }
 
-// Struct for serializing the response
+// Struct for serializing the io decryption response
 #[derive(Debug, Serialize, Deserialize)]
 pub struct IoDecryptionResponse {
     pub decrypted_data: Vec<u8>,
