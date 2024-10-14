73577254fa8bf4401ce5f58cac3f35fb12627bd4
ssolit <49683577+ssolit@users.noreply.github.com>
2024-10-14 17:38:42 -0400

    Io tx (#1)
    Co-authored-by: ssolit <sol@seismic.systems>
    Co-authored-by: Christian Drappi <c@seismic.systems>

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+mod tx_io;
+
+use anyhow::{Context, Result};
+use attestation_agent::{AttestationAPIs, AttestationAgent};
+use log::debug;
+
+use hyper::{Body, Request, Response, Server, StatusCode};
+use routerify::{prelude::*, Middleware, RequestInfo, Router, RouterService};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use tx_io::handlers::*;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Initialize the logger
+    env_logger::init();
+
+    // configure the attestation agent
+    // Not currently used or configured, but it will be later on
+    let aa = AttestationAgent::new(None).context("Failed to create an AttestationAgent")?;
+    debug!("Detected TEE type: {:?}", aa.get_tee_type());
+
+    // create the server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 7878));
+    let router = Router::builder()
+        .middleware(Middleware::pre(logger))
+        .post("/tx_io/encrypt", tx_io_encrypt_handler)
+        .post("/tx_io/decrypt", tx_io_decrypt_handler)
+        .err_handler_with_info(error_handler)
+        .build()
+        .unwrap();
+    let service = RouterService::new(router).unwrap();
+    let server = Server::bind(&addr).serve(service);
+
+    println!("Listening on http://{}\n", addr);
+
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
+    }
+
+    Ok(())
+}
+
+// A middleware which logs an http request.
+async fn logger(req: Request<Body>) -> Result<Request<Body>, Infallible> {
+    println!(
+        "{} {} {}",
+        req.remote_addr(),
+        req.method(),
+        req.uri().path()
+    );
+    Ok(req)
+}
+
+// Define an error handler function which will accept the `routerify::Error`
+// and the request information and generates an appropriate response.
+async fn error_handler(err: routerify::RouteError, _: RequestInfo) -> Response<Body> {
+    println!("\n\nError: {:?}\n\n", err);
+    eprintln!("{}", err);
+    Response::builder()
+        .status(StatusCode::INTERNAL_SERVER_ERROR)
+        .body(Body::from(format!("Something went wrong: {}", err)))
+        .unwrap()
+}
---
+++ src/tx_io/handlers.rs
@@ -0,0 +1,146 @@
+use hyper::{body::to_bytes, Body, Request, Response};
+use secp256k1::ecdh::SharedSecret;
+use secp256k1::SecretKey;
+use std::convert::Infallible;
+
+use crate::tx_io::structs::*;
+use crate::tx_io::utils::*;
+
+/// Handles an IO encryption request, encrypting the provided data using AES.
+///
+/// # Arguments
+/// * `req` - The incoming HTTP request containing the data to be encrypted. The body of the request
+/// should be a JSON-encoded `IoEncryptionRequest`.
+///
+/// # Returns
+/// A `Result` containing an HTTP response with the encrypted data, or an error of type `Infallible`.
+/// The response body is JSON-encoded and contains the encrypted data as part of an `IoEncryptionResponse`.
+///
+/// # Errors
+/// The function may panic if parsing the request body, creating the shared secret, or encrypting the data fails.
+pub async fn tx_io_encrypt_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    // parse the request body
+    let body_bytes = to_bytes(req.into_body()).await.unwrap();
+    let encryption_request: IoEncryptionRequest = serde_json::from_slice(&body_bytes).unwrap();
+
+    // load key and encrypt data
+    let ecdh_sk = get_secp256k1_sk();
+    let shared_secret = SharedSecret::new(&encryption_request.msg_sender, &ecdh_sk);
+    let aes_key = derive_aes_key(&shared_secret).unwrap();
+    let encrypted_data = aes_encrypt(&aes_key, &encryption_request.data, encryption_request.nonce);
+
+    let response_body = IoEncryptionResponse { encrypted_data };
+    let response_json = serde_json::to_string(&response_body).unwrap();
+
+    Ok(Response::new(Body::from(response_json)))
+}
+
+/// Handles an IO decryption request, decrypting the provided encrypted data using AES.
+///
+/// # Arguments
+/// * `req` - The incoming HTTP request containing the encrypted data. The body of the request
+/// should be a JSON-encoded `IoDecryptionRequest`.
+///
+/// # Returns
+/// A `Result` containing an HTTP response with the decrypted data, or an error of type `Infallible`.
+/// The response body is JSON-encoded and contains the decrypted data as part of an `IoDecryptionResponse`.
+///
+/// # Errors
+/// The function may panic if parsing the request body, creating the shared secret, or decrypting the data fails.
+pub async fn tx_io_decrypt_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    // parse the request body
+    let body_bytes = to_bytes(req.into_body()).await.unwrap();
+    let decryption_request: IoDecryptionRequest = serde_json::from_slice(&body_bytes).unwrap();
+
+    // load key and decrypt data
+    let ecdh_sk = get_secp256k1_sk();
+    let shared_secret = SharedSecret::new(&decryption_request.msg_sender, &ecdh_sk);
+    let aes_key = derive_aes_key(&shared_secret).unwrap();
+    let decrypted_data = aes_decrypt(&aes_key, &decryption_request.data, decryption_request.nonce);
+
+    let response_body = IoDecryptionResponse { decrypted_data };
+    let response_json = serde_json::to_string(&response_body).unwrap();
+
+    Ok(Response::new(Body::from(response_json)))
+}
+
+/// Loads a secp256k1 private key from a file.
+///
+/// This function reads the keypair from a JSON file for testing purposes. Eventually, it should
+/// be replaced with a more secure solution, such as requesting a key from a KMS service.
+///
+/// # Returns
+/// A secp256k1 `SecretKey` loaded from the keypair file.
+///
+/// # Panics
+/// The function may panic if the file is missing or if it cannot deserialize the keypair.
+fn get_secp256k1_sk() -> SecretKey {
+    read_secp256k1_keypair("./src/tx_io/ex_keypair.json")
+        .unwrap()
+        .secret_key
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hyper::{Body, Request};
+    use secp256k1::PublicKey;
+    use std::str::FromStr;
+
+    #[tokio::test]
+    async fn test_io_encryption() {
+        // Prepare encryption request body
+        let base_url = "http://localhost:7878";
+        let data_to_encrypt = vec![72, 101, 108, 108, 111];
+        let encryption_request = IoEncryptionRequest {
+            msg_sender: PublicKey::from_str(
+                "03e31e68908a6404a128904579c677534d19d0e5db80c7d9cf4de6b4b7fe0518bd",
+            )
+            .unwrap(),
+            data: data_to_encrypt.clone(),
+            nonce: 12345678,
+        };
+        let payload_json = serde_json::to_string(&encryption_request).unwrap();
+
+        let req = Request::builder()
+            .method("POST")
+            .uri(format!("{}/tx_io/encrypt", base_url))
+            .header("Content-Type", "application/json")
+            .body(Body::from(payload_json))
+            .unwrap();
+
+        let res = tx_io_encrypt_handler(req).await.unwrap();
+        assert_eq!(res.status(), 200);
+
+        // Parse the response body
+        let body = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        println!("body: {:?}", body);
+        let enc_response: IoEncryptionResponse = serde_json::from_slice(&body).unwrap();
+        assert!(!enc_response.encrypted_data.is_empty());
+
+        // check that decryption returns the original data
+        // Prepare decrypt request body
+        let decryption_request = IoDecryptionRequest {
+            msg_sender: PublicKey::from_str(
+                "03e31e68908a6404a128904579c677534d19d0e5db80c7d9cf4de6b4b7fe0518bd",
+            )
+            .unwrap(),
+            data: enc_response.encrypted_data,
+            nonce: 12345678,
+        };
+        let payload_json = serde_json::to_string(&decryption_request).unwrap();
+        let req = Request::builder()
+            .method("POST")
+            .uri(format!("{}/tx_io/decrypt", base_url))
+            .header("Content-Type", "application/json")
+            .body(Body::from(payload_json))
+            .unwrap();
+
+        let res = tx_io_decrypt_handler(req).await.unwrap();
+        assert_eq!(res.status(), 200);
+
+        let body = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let dec_response: IoDecryptionResponse = serde_json::from_slice(&body).unwrap();
+        assert_eq!(dec_response.decrypted_data, data_to_encrypt);
+    }
+}
