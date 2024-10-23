ae1785a0d0816ac8664913919769030178b0f168
ssolit <49683577+ssolit@users.noreply.github.com>
2024-10-23 14:50:06 -0400

    Genesis data (#7)
    Co-authored-by: ssolit <sol@seismic.systems>

--- src/coco_aa/structs.rs
+++ src/coco_aa/structs.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct AttestationGetEvidenceRequest {
+    // For AzTdxVtpm, this affects the quotes's aztdxvtpm.quote.body.report_data
     pub runtime_data: Vec<u8>,
 }
 
