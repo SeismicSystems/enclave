f1ee216bbe693a5679c0c9121b88479892fc63d4
Christian Drappi <c@seismic.systems>
2024-10-30 15:44:01 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -45,14 +45,14 @@ async fn main() -> Result<()> {
             "/attestation/aa/get_evidence",
             attestation_get_evidence_handler,
         )
-        .post("/signing/sign", secp256k1_sign_handler)
-        .post("/signing/verify", secp256k1_verify_handler)
-        .post("/tx_io/encrypt", tx_io_encrypt_handler)
-        .post("/tx_io/decrypt", tx_io_decrypt_handler)
         .post(
             "/attestation/as/eval_evidence",
             attestation_eval_evidence_handler,
         )
+        .post("/signing/sign", secp256k1_sign_handler)
+        .post("/signing/verify", secp256k1_verify_handler)
+        .post("/tx_io/encrypt", tx_io_encrypt_handler)
+        .post("/tx_io/decrypt", tx_io_decrypt_handler)
         .err_handler_with_info(error_handler)
         .build()
         .unwrap();
