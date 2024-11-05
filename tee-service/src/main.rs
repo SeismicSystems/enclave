34984449092c737e846c7991bcbcb7bb82fc593a
Ubuntu <azureuser@tdx-base-test.3z0tm1yprcsefaq3x0wq2wcmyg.cx.internal.cloudapp.net>
2024-11-05 16:44:01 +0000

    expose api to public, add repository fields

--- tee-service/src/main.rs
+++ tee-service/src/main.rs
@@ -6,7 +6,10 @@ use tee_service::server::start_server;
 /// Initializes a server with the given address and handlers
 #[tokio::main]
 async fn main() -> Result<()> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 7878));
+    // 0.0.0.0 exposes to public internet
+    // 127.0.0.1 will only allow other processes on machine to ping
+    let addr = SocketAddr::from(([0, 0, 0, 0], 7878));
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 7878));
     start_server(addr).await?;
     Ok(())
 }
