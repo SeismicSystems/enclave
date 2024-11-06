f620fa423b70bda5ccb2c78c628a6b0d959bffd1
ssolit <sol@seismic.systems>
2024-11-06 18:38:41 +0000

    modified tee-service main to import the default endpoint from tee-service-spi

--- tee-service-api/src/client/http_client.rs
+++ tee-service-api/src/client/http_client.rs
@@ -2,6 +2,8 @@ use super::*;
 use reqwest::Client;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
+// Unspecified (0.0.0.0) exposes to public internet
+// Localhost (127.0.0.1) will only allow other processes on machine to ping
 pub const TEE_DEFAULT_ENDPOINT_ADDR: IpAddr = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
 pub const TEE_DEFAULT_ENDPOINT_PORT: u16 = 7878;
 
