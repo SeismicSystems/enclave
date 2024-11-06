0559d61f9f61e7c80a03fd0f84026713f1653713
ssolit <sol@seismic.systems>
2024-11-06 17:33:31 +0000

    derive clone for TEE request structs. useful for benchmarking code

--- tee-service-api/src/request_types/coco_aa.rs
+++ tee-service-api/src/request_types/coco_aa.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct AttestationGetEvidenceRequest {
     // For AzTdxVtpm, this affects the quotes's aztdxvtpm.quote.body.report_data
     pub runtime_data: Vec<u8>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct AttestationGetEvidenceResponse {
     pub evidence: Vec<u8>,
 }
