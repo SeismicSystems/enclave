c492aa7bc2b8a4537831ee15d515ce2b039b90a3
ssolit <sol@seismic.systems>
2024-11-06 18:52:12 +0000

    modified main to actually use default

--- tee-service/src/main.rs
+++ tee-service/src/main.rs
@@ -2,14 +2,12 @@ use anyhow::Result;
 use std::net::SocketAddr;
 
 use tee_service::server::start_server;
+use tee_service_api::client::http_client::{TEE_DEFAULT_ENDPOINT_ADDR, TEE_DEFAULT_ENDPOINT_PORT};
 
 /// Initializes a server with the given address and handlers
 #[tokio::main]
 async fn main() -> Result<()> {
-    // 0.0.0.0 exposes to public internet
-    // 127.0.0.1 will only allow other processes on machine to ping
-    let addr = SocketAddr::from(([0, 0, 0, 0], 7878));
-    // let addr = SocketAddr::from(([127, 0, 0, 1], 7878));
+    let addr = SocketAddr::from((TEE_DEFAULT_ENDPOINT_ADDR, TEE_DEFAULT_ENDPOINT_PORT));
     start_server(addr).await?;
     Ok(())
 }
