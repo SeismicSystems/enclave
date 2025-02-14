a1a091769e3654dfdf82ab5ec7364ce78717a261
ssolit <sol@seismic.systems>
2025-02-14 17:41:10 +0000

    added a test for server.rs to actually launch the server and make a request

--- crates/enclave-server/src/server.rs
+++ crates/enclave-server/src/server.rs
@@ -9,31 +9,13 @@ use anyhow::Result;
 use http_body_util::Full;
 use hyper::{
     body::{Body, Bytes},
-    server::conn::{http1, http2},
+    server::conn::http1,
     service::service_fn,
     Method, Request, Response, StatusCode,
 };
 use hyper_util::rt::TokioIo;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
-
-// #[derive(Clone)]
-// // An Executor that uses the tokio runtime.
-// pub struct TokioExecutor;
-
-// // Implement the `hyper::rt::Executor` trait for `TokioExecutor` so that it can be used to spawn
-// // tasks in the hyper runtime.
-// // An Executor allows us to manage execution of tasks which can help us improve the efficiency and
-// // scalability of the server.
-// impl<F> hyper::rt::Executor<F> for TokioExecutor
-// where
-//     F: std::future::Future + Send + 'static,
-//     F::Output: Send + 'static,
-// {
-//     fn execute(&self, fut: F) {
-//         tokio::task::spawn(fut);
-//     }
-// }
 
 pub async fn start_server(addr: SocketAddr) -> Result<()> {
     // Initialize services
@@ -60,6 +42,9 @@ pub async fn start_server(addr: SocketAddr) -> Result<()> {
 pub async fn route_req(req: Request<impl Body>) -> Result<Response<Full<Bytes>>> {
     log_request(&req);
     match (req.method(), req.uri().path()) {
+        // Health check
+        (&Method::GET, "/health") => health_check(req).await,
+
         // Genesis
         (&Method::GET, "/genesis/data") => genesis_get_data_handler(req).await,
 
@@ -91,9 +76,79 @@ pub async fn route_req(req: Request<impl Body>) -> Result<Response<Full<Bytes>>>
 }
 
 
+async fn health_check(_: Request<impl Body>) -> Result<Response<Full<Bytes>>, anyhow::Error> {
+    Ok(Response::new(Full::new(Bytes::from("OK"))))
+}
+
 fn log_request(req: &Request<impl Body>) {
     let method = req.method().to_string();
     let uri = req.uri().to_string();
     let log = format!("{} {}", method, uri);
     println!("{log}");
+}
+
+#[cfg(test)]
+mod test {
+    use super::start_server;
+    use seismic_enclave::client::http_client::TeeHttpClient;
+    use seismic_enclave::client::http_client::{TEE_DEFAULT_ENDPOINT_ADDR, TEE_DEFAULT_ENDPOINT_PORT};
+    use seismic_enclave::request_types::tx_io::*;
+    use seismic_enclave::TeeAPI;
+
+    use std::net::SocketAddr;
+    use secp256k1::PublicKey;
+    use std::str::FromStr;
+
+    use tokio::time::Duration;
+    use tokio::time::Instant;
+    
+    #[tokio::test]
+    async fn test_tx_io_req() {
+        // spawn a seperate thread for the server, otherwise the test will hang
+        let addr = SocketAddr::from((TEE_DEFAULT_ENDPOINT_ADDR, TEE_DEFAULT_ENDPOINT_PORT));
+        let _server_handle = tokio::spawn(start_server(addr)); 
+        wait_for_server(&format!("{}/health", addr), Duration::from_secs(2)).await.unwrap();
+
+        // make the request struct
+        let data_to_encrypt = vec![72, 101, 108, 108, 111];
+        let mut nonce = vec![0u8; 4]; // 4 leading zeros
+        nonce.extend_from_slice(&(12345678u64).to_be_bytes()); // Append the 8-byte u64
+        let encryption_request = IoEncryptionRequest {
+            key: PublicKey::from_str(
+                "03e31e68908a6404a128904579c677534d19d0e5db80c7d9cf4de6b4b7fe0518bd",
+            )
+            .unwrap(),
+            data: data_to_encrypt.clone(),
+            nonce: nonce.clone().into(),
+        };
+
+        // make the http request
+        let http_cleint = TeeHttpClient::default();
+        let encryption_response = http_cleint.tx_io_encrypt(encryption_request).await.unwrap();
+
+        // check the response
+        assert!(!encryption_response.encrypted_data.is_empty());
+
+        let decryption_request = IoDecryptionRequest {
+            key: PublicKey::from_str(
+                "03e31e68908a6404a128904579c677534d19d0e5db80c7d9cf4de6b4b7fe0518bd",
+            )
+            .unwrap(),
+            data: encryption_response.encrypted_data,
+            nonce: nonce.into(),
+        };
+        let decryption_response = http_cleint.tx_io_decrypt(decryption_request).await.unwrap();
+        assert_eq!(decryption_response.decrypted_data, data_to_encrypt);
+    }
+
+    async fn wait_for_server(addr: &str, timeout: Duration) -> Result<(), Box<dyn std::error::Error>> {
+        let start = Instant::now();
+        while start.elapsed() < timeout {
+            if reqwest::get(addr).await.is_ok() {
+                return Ok(());
+            }
+            tokio::time::sleep(Duration::from_millis(50)).await;
+        }
+        Err("Server did not start in time".into())
+    }
 }
