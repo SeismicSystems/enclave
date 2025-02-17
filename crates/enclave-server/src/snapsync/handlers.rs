15d4850dcf17b670999311032e506bd8aea939e6
phexyz <32248504+phexyz@users.noreply.github.com>
2025-02-17 16:27:25 -0500

    last commit

--- crates/enclave-server/src/snapsync/handlers.rs
+++ crates/enclave-server/src/snapsync/handlers.rs
@@ -148,12 +148,8 @@ mod tests {
     use serial_test::serial;
 
     use crate::{
-        coco_aa::attest_signing_pk,
-        // coco_as::handlers::attestation_eval_evidence_handler, coco_as::into_original::*,
-        init_as_policies,
-        init_coco_aa,
-        init_coco_as,
-        utils::test_utils::is_sudo,
+        coco_aa::attest_signing_pk, coco_aa::init_coco_aa, coco_as::init_as_policies,
+        coco_as::init_coco_as, utils::test_utils::is_sudo,
     };
 
     #[serial(attestation_agent, attestation_service)]
@@ -187,18 +183,9 @@ mod tests {
             policy_ids: vec!["allow".to_string()],
         };
 
-        let payload_json = serde_json::to_string(&snap_sync_request).unwrap();
-
-        let req: Request<Full<Bytes>> = Request::builder()
-            .method("POST")
-            .uri("/")
-            .body(Full::from(Bytes::from(payload_json)))
+        let snapsync_response = rpc_provide_snapsync_handler(snap_sync_request)
+            .await
             .unwrap();
-        let res: Response<Full<Bytes>> = provide_snapsync_handler(req).await.unwrap();
-        assert_eq!(res.status(), StatusCode::OK, "{res:?}");
-
-        let body: Bytes = res.into_body().collect().await.unwrap().to_bytes();
-        let snapsync_response: SnapSyncResponse = serde_json::from_slice(&body).unwrap();
 
         // Check that you can decrypt the response successfully
         let server_pk =
@@ -256,13 +243,9 @@ mod tests {
             policy_ids: vec!["allow".to_string()],
         };
 
-        let payload_json = serde_json::to_string(&snap_sync_request).unwrap();
-        let req: Request<Full<Bytes>> = Request::builder()
-            .method("POST")
-            .uri("/")
-            .body(Full::from(Bytes::from(payload_json)))
-            .unwrap();
-        let res: Response<Full<Bytes>> = provide_snapsync_handler(req).await.unwrap();
-        assert_eq!(res.status(), StatusCode::BAD_REQUEST);
+        let res = rpc_provide_snapsync_handler(snap_sync_request).await;
+        assert_eq!(res.is_err(), true);
+        let err = res.err().unwrap();
+        assert!(err.to_string().contains("Error while evaluating evidence"));
     }
 }
