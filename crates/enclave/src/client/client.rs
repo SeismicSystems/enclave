0fbe4c9a8991f4dfe3c76e44b00c154a0fe2cd86
phexyz <32248504+phexyz@users.noreply.github.com>
2025-02-18 14:41:37 -0500

    last commit

---
+++ crates/enclave/src/client/client.rs
@@ -0,0 +1,41 @@
+use jsonrpsee::http_client::HttpClient;
+use std::{
+    net::{IpAddr, Ipv4Addr},
+    ops::Deref,
+};
+
+pub const TEE_DEFAULT_ENDPOINT_ADDR: IpAddr = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
+pub const TEE_DEFAULT_ENDPOINT_PORT: u16 = 7878;
+
+/// A client for the enclave API.
+pub struct EnclaveClient {
+    /// The inner HTTP client.
+    inner: HttpClient,
+}
+
+impl Default for EnclaveClient {
+    fn default() -> Self {
+        Self::new(format!(
+            "http://{}:{}",
+            TEE_DEFAULT_ENDPOINT_ADDR, TEE_DEFAULT_ENDPOINT_PORT
+        ))
+    }
+}
+
+impl Deref for EnclaveClient {
+    type Target = HttpClient;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl EnclaveClient {
+    /// Create a new enclave client.
+    pub fn new(url: impl AsRef<str>) -> Self {
+        let inner = jsonrpsee::http_client::HttpClientBuilder::default()
+            .build(url)
+            .unwrap();
+        Self { inner }
+    }
+}
