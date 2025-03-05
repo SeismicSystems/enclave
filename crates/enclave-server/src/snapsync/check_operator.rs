d5c48cda68262837a7f767fc541266ed8fe2837f
ssolit <sol@seismic.systems>
2025-03-05 22:20:42 +0000

    check_operator test hits reth

--- crates/enclave-server/src/snapsync/check_operator.rs
+++ crates/enclave-server/src/snapsync/check_operator.rs
@@ -1,8 +1,7 @@
-use alloy_sol_types::sol;
-use alloy_provider::ProviderBuilder;
 use alloy_primitives::{Address, Bytes};
+use alloy_provider::ProviderBuilder;
+use alloy_sol_types::sol;
 use std::sync::Arc;
-
 
 const OPERATOR_ADDR: &str = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // TODO: replace
 
@@ -14,13 +13,19 @@ sol! {
     }
 }
 
-pub async fn check_operator(rootfs_hash: Bytes, mrtd: Bytes, rtmr0: Bytes, rtmr3: Bytes) -> Result<bool, anyhow::Error> {
+pub async fn check_operator(
+    rootfs_hash: Bytes,
+    mrtd: Bytes,
+    rtmr0: Bytes,
+    rtmr3: Bytes,
+) -> Result<bool, anyhow::Error> {
     // Set up the provider to connect to the local Ethereum node
-    let provider = ProviderBuilder::new()
-        .on_http("http://localhost:8545".parse()?);
+    let provider = ProviderBuilder::new().on_http("http://localhost:8545".parse()?);
 
     // Specify the contract address
-    let contract_address: Address = OPERATOR_ADDR.parse().expect("const OPERATOR_ADDR does not parse");
+    let contract_address: Address = OPERATOR_ADDR
+        .parse()
+        .expect("const OPERATOR_ADDR does not parse");
 
     // Instantiate the contract
     let contract = UpgradeOperator::new(contract_address, Arc::new(provider));
@@ -30,7 +35,7 @@ pub async fn check_operator(rootfs_hash: Bytes, mrtd: Bytes, rtmr0: Bytes, rtmr3
         .get_mrtd(rootfs_hash, mrtd, rtmr0, rtmr3)
         .call()
         .await?;
-    let is_valid: bool = result._0; 
+    let is_valid: bool = result._0;
 
     // Output the result
     println!("get_mrtd result: {:?}", is_valid);
@@ -44,13 +49,13 @@ mod tests {
 
     #[tokio::test]
     async fn test_check_operator() {
-        let thing: Bytes = Bytes::from("00");
-        let _result = check_operator(
-            thing.clone(),
-            thing.clone(),
-            thing.clone(),
-            thing.clone(),
-        ).await.unwrap();
-    }
+        let rootfs_hash = Bytes::from(vec![0x00; 32]);
+        let mrtd = Bytes::from(vec![0x00; 48]);
+        let rtmr0 = Bytes::from(vec![0x00; 48]);
+        let rtmr3 = Bytes::from(vec![0x00; 48]);
 
+        let _result = check_operator(rootfs_hash, mrtd, rtmr0, rtmr3)
+            .await
+            .unwrap();
+    }
 }
