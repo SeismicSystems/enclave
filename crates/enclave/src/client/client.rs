146d11ee4577e0f600d3b10349441a9929074226
phexyz <32248504+phexyz@users.noreply.github.com>
2025-03-12 14:13:17 -0400

    last commit

--- crates/enclave/src/client/client.rs
+++ crates/enclave/src/client/client.rs
@@ -73,12 +73,19 @@ impl EnclaveClient {
     }
 }
 
+// Helper function to convert any error to jsonrpsee::core::client::Error
+fn convert_error<T, E: std::fmt::Display>(
+    result: Result<T, E>,
+) -> Result<T, jsonrpsee::core::client::Error> {
+    result.map_err(|e| jsonrpsee::core::client::Error::Custom(e.to_string()))
+}
+
 macro_rules! impl_sync_client {
-    ($(fn $method_name:ident(&self $(, $param:ident: $param_ty:ty)*) -> $return_ty:ty),* $(,)?) => {
+    ($(fn $method_name:ident(&self $(, $param:ident: $param_ty:ty)*) -> Result<$return_ty:ty, $error_ty:ty>),* $(,)?) => {
         impl SyncEnclaveApiClient for EnclaveClient {
             $(
-                fn $method_name(&self, $($param: $param_ty),*) -> $return_ty {
-                    self.runtime.block_on(self.async_client.$method_name($($param),*))
+                fn $method_name(&self, $($param: $param_ty),*) -> Result<$return_ty, jsonrpsee::core::client::Error> {
+                    convert_error(self.runtime.block_on(self.async_client.$method_name($($param),*)))
                 }
             )+
         }
@@ -98,76 +105,3 @@ impl_sync_client!(
     fn get_attestation_evidence(&self, _req: AttestationGetEvidenceRequest) -> Result<AttestationGetEvidenceResponse, ClientError>,
     fn eval_attestation_evidence(&self, _req: AttestationEvalEvidenceRequest) -> Result<AttestationEvalEvidenceResponse, ClientError>,
 );
-
-struct MockEnclaveClient;
-
-impl SyncEnclaveApiClient for MockEnclaveClient {
-    fn health_check(&self) -> Result<String, RpcError> {
-        Ok("OK".to_string())
-    }
-
-    fn get_eph_rng_keypair(&self) -> Result<schnorrkel::keys::Keypair, RpcError> {
-        // Return a sample Schnorrkel keypair for testing
-        Ok(get_unsecure_sample_schnorrkel_keypair())
-    }
-
-    fn encrypt(&self, req: IoEncryptionRequest) -> Result<IoEncryptionResponse, RpcError> {
-        // Use the sample secret key for encryption
-        let encrypted_data = ecdh_encrypt(
-            &req.key,
-            &get_unsecure_sample_secp256k1_sk(),
-            &req.data,
-            req.nonce,
-        )
-        .unwrap();
-
-        Ok(IoEncryptionResponse { encrypted_data })
-    }
-
-    fn decrypt(&self, req: IoDecryptionRequest) -> Result<IoDecryptionResponse, RpcError> {
-        // Use the sample secret key for decryption
-        let decrypted_data = ecdh_decrypt(
-            &req.key,
-            &get_unsecure_sample_secp256k1_sk(),
-            &req.data,
-            req.nonce,
-        )
-        .map_err(|e| RpcError::Custom(e.to_string()))?;
-
-        Ok(IoDecryptionResponse { decrypted_data })
-    }
-
-    fn get_public_key(&self) -> Result<secp256k1::PublicKey, RpcError> {
-        Ok(get_unsecure_sample_secp256k1_pk())
-    }
-
-    fn get_genesis_data(&self) -> Result<GenesisDataResponse, RpcError> {
-        unimplemented!("genesis_get_data not implemented for mock server")
-    }
-
-    fn get_snapsync_backup(&self, _req: SnapSyncRequest) -> Result<SnapSyncResponse, RpcError> {
-        unimplemented!("provide_snapsync_backup not implemented for mock server")
-    }
-
-    fn sign(&self, _req: Secp256k1SignRequest) -> Result<Secp256k1SignResponse, RpcError> {
-        unimplemented!("secp256k1_sign not implemented for mock server")
-    }
-
-    fn verify(&self, _req: Secp256k1VerifyRequest) -> Result<Secp256k1VerifyResponse, RpcError> {
-        unimplemented!("secp256k1_verify not implemented for mock server")
-    }
-
-    fn get_attestation_evidence(
-        &self,
-        _req: AttestationGetEvidenceRequest,
-    ) -> Result<AttestationGetEvidenceResponse, RpcError> {
-        unimplemented!("attestation_get_evidence not implemented for mock server")
-    }
-
-    fn eval_attestation_evidence(
-        &self,
-        _req: AttestationEvalEvidenceRequest,
-    ) -> Result<AttestationEvalEvidenceResponse, RpcError> {
-        unimplemented!("attestation_eval_evidence not implemented for mock server")
-    }
-}
