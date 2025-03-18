8e18a1e02a874afb26b2b108f758c58b346b2b6e
phexyz <32248504+phexyz@users.noreply.github.com>
2025-03-18 12:02:17 -0400

    Delete parsing from u64 into nonce for security  (#97)

--- crates/enclave/src/request_types/nonce.rs
+++ crates/enclave/src/request_types/nonce.rs
@@ -1,76 +1,31 @@
-use aes_gcm::{
-    aead::{generic_array::GenericArray, AeadCore},
-    Aes256Gcm,
-};
+use rand::RngCore;
 use serde::{Deserialize, Serialize};
+use sha2::digest::{consts::U12, generic_array::GenericArray};
 
-use crate::u64_to_be_bytes_array;
+pub const AESGCM_NONCE_SIZE: usize = 12; // Size of AES-GCM nonce in bytes
 
+/// The intermediate type to represent a nonce in the enclave
 #[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(untagged)]
-pub enum Nonce {
-    U64(u64),
-    Vec(Vec<u8>),
-    Bytes([u8; 12]),
-}
+pub struct Nonce(pub [u8; AESGCM_NONCE_SIZE]);
 
-impl From<u64> for Nonce {
-    fn from(value: u64) -> Self {
-        Self::U64(value)
+impl Nonce {
+    pub fn new_rand() -> Self {
+        let mut rng = rand::rng();
+        // Generate a random U96 value
+        let mut bytes = [0u8; 12]; // 96 bits = 12 bytes
+        rng.fill_bytes(&mut bytes);
+        Nonce(bytes)
     }
 }
 
-impl From<Vec<u8>> for Nonce {
-    fn from(value: Vec<u8>) -> Self {
-        Self::Vec(value)
+impl From<Nonce> for aes_gcm::Nonce<U12> {
+    fn from(nonce: Nonce) -> Self {
+        GenericArray::clone_from_slice(&nonce.0)
     }
 }
 
-impl From<[u8; 12]> for Nonce {
-    fn from(value: [u8; 12]) -> Self {
-        Self::Bytes(value)
-    }
-}
-
-impl Into<Vec<u8>> for Nonce {
-    fn into(self) -> Vec<u8> {
-        match self {
-            Self::U64(value) => u64_to_be_bytes_array(value).to_vec(),
-            Self::Vec(value) => value,
-            Self::Bytes(value) => value.to_vec(),
-        }
-    }
-}
-
-impl TryInto<GenericArray<u8, <Aes256Gcm as AeadCore>::NonceSize>> for Nonce {
-    type Error = anyhow::Error;
-
-    fn try_into(
-        self,
-    ) -> Result<GenericArray<u8, <Aes256Gcm as AeadCore>::NonceSize>, anyhow::Error> {
-        let nonce_vec: Vec<u8> = self.into();
-        if nonce_vec.len() != 12 {
-            return Err(anyhow::anyhow!("Nonce must be exactly 12 bytes (92 bits)"));
-        }
-        Ok(GenericArray::<u8, <Aes256Gcm as AeadCore>::NonceSize>::clone_from_slice(&nonce_vec))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_nonce_into_vec_u8() {
-        // Test u64 value of 1
-        let nonce_u64 = Nonce::U64(1);
-        let vec_from_u64: Vec<u8> = nonce_u64.into();
-
-        // Test byte representation of 1 left-padded to 12 bytes
-        let bytes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1];
-        let nonce_bytes = Nonce::Bytes(bytes);
-        let vec_from_bytes: Vec<u8> = nonce_bytes.into();
-        // u64 value 1 as big-endian bytes should be [0, 0, 0, 0, 0, 0, 0, 1]
-        assert_eq!(vec_from_bytes, vec_from_u64);
+impl From<[u8; AESGCM_NONCE_SIZE]> for Nonce {
+    fn from(bytes: [u8; AESGCM_NONCE_SIZE]) -> Self {
+        Nonce(bytes)
     }
 }
