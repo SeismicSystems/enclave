bc3322fc49639063ebac1df655bd187ddce4b321
ssolit <sol@seismic.systems>
2025-03-25 20:40:41 +0000

    removed operator shares code from builder

--- crates/enclave-server/src/key_manager/builder.rs
+++ crates/enclave-server/src/key_manager/builder.rs
@@ -6,10 +6,6 @@ use rand::TryRngCore;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
-// MasterKey Constants
-const TEE_DOMAIN_SEPARATOR: &[u8] = b"seismic-tee-domain-separator";
-const MASTER_KEY_DOMAIN_INFO: &[u8] = b"seismic-master-key-derivation";
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct OperatorShare {
     pub share: [u8; 32],
@@ -30,28 +26,13 @@ impl FromStr for OperatorShare {
 }
 
 pub struct KeyManagerBuilder {
-    operator_shares: Vec<OperatorShare>,
 }
 
 impl KeyManagerBuilder {
     pub fn new() -> Self {
         Self {
-            operator_shares: Vec::new(),
         }
     }
-
-    // // For now, there is one other share at most.
-    // pub fn with_operator_share(mut self, share: OperatorShare) -> Self {
-    //     self.operator_shares.push(share);
-    //     self
-    // }
-
-    // pub fn build_from_operator_shares(self) -> Result<KeyManager> {
-    //     let master_key_bytes = [0u8; 32];
-    //     // derive master key from shares
-    //     let km = KeyManager::new(master_key_bytes)?;
-    //     Ok(km)
-    // }
 
     pub fn build_from_os_rng() -> Result<KeyManager> {
         let mut rng = OsRng;
