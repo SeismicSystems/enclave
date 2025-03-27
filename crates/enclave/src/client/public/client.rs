3088010ae76f1d4549694e897d98df6048bc2760
ssolit <sol@seismic.systems>
2025-03-27 13:23:49 +0000

    public client

--- crates/enclave/src/client/public/client.rs
+++ crates/enclave/src/client/public/client.rs
@@ -2,33 +2,22 @@ use jsonrpsee::{core::ClientError, http_client::HttpClient};
 use std::{ops::Deref, time::Duration};
 use tokio::runtime::{Handle, Runtime};
 
-use super::{EnclaveInternalAPIClient, SyncEnclaveInternalAPIClient};
+use super::{EnclavePublicAPIClient, SyncEnclavePublicAPIClient};
 use crate::client::builder::BuildableClient;
 use crate::client::{
     ENCLAVE_CLIENT_RUNTIME, ENCLAVE_DEFAULT_ENDPOINT_ADDR, ENCLAVE_DEFAULT_INTERNAL_PORT,
 };
 
-/// request structs for internal api
-use crate::{
-    coco_aa::{AttestationGetEvidenceRequest, AttestationGetEvidenceResponse},
-    coco_as::{AttestationEvalEvidenceRequest, AttestationEvalEvidenceResponse},
-    signing::{
-        Secp256k1SignRequest, Secp256k1SignResponse, Secp256k1VerifyRequest,
-        Secp256k1VerifyResponse,
-    },
-    tx_io::{IoDecryptionRequest, IoDecryptionResponse, IoEncryptionRequest, IoEncryptionResponse},
-};
-
 /// A client for the enclave API.
 #[derive(Debug, Clone)]
-pub struct EnclaveInternalClient {
+pub struct EnclavePublicClient {
     /// The inner HTTP client.
     async_client: HttpClient,
     /// The runtime for the client.
     handle: Handle,
 }
 
-impl Default for EnclaveInternalClient {
+impl Default for EnclavePublicClient {
     fn default() -> Self {
         let url = format!(
             "http://{}:{}",
@@ -41,14 +30,14 @@ impl Default for EnclaveInternalClient {
         Self::new_from_client(async_client)
     }
 }
-impl Deref for EnclaveInternalClient {
+impl Deref for EnclavePublicClient {
     type Target = HttpClient;
 
     fn deref(&self) -> &Self::Target {
         &self.async_client
     }
 }
-impl BuildableClient for EnclaveInternalClient {
+impl BuildableClient for EnclavePublicClient {
     fn new_from_client(async_client: HttpClient) -> Self {
         let handle = Handle::try_current().unwrap_or_else(|_| {
             let runtime = ENCLAVE_CLIENT_RUNTIME.get_or_init(|| Runtime::new().unwrap());
@@ -70,7 +59,7 @@ impl BuildableClient for EnclaveInternalClient {
 
 macro_rules! impl_sync_client_trait {
     ($(fn $method_name:ident(&self $(, $param:ident: $param_ty:ty)*) -> $return_ty:ty),* $(,)?) => {
-        impl SyncEnclaveInternalAPIClient for EnclaveInternalClient {
+        impl SyncEnclavePublicAPIClient for EnclavePublicClient {
             $(
                 fn $method_name(&self, $($param: $param_ty),*) -> $return_ty {
                     self.block_on_with_runtime(self.async_client.$method_name($($param),*))
@@ -81,11 +70,6 @@ macro_rules! impl_sync_client_trait {
 }
 
 impl_sync_client_trait!(
-    fn encrypt(&self, req: IoEncryptionRequest) -> Result<IoEncryptionResponse, ClientError>,
-    fn decrypt(&self, req: IoDecryptionRequest) -> Result<IoDecryptionResponse, ClientError>,
-    fn sign(&self, _req: Secp256k1SignRequest) -> Result<Secp256k1SignResponse, ClientError>,
-    fn verify(&self, _req: Secp256k1VerifyRequest) -> Result<Secp256k1VerifyResponse, ClientError>,
-    fn get_attestation_evidence(&self, _req: AttestationGetEvidenceRequest) -> Result<AttestationGetEvidenceResponse, ClientError>,
-    fn eval_attestation_evidence(&self, _req: AttestationEvalEvidenceRequest) -> Result<AttestationEvalEvidenceResponse, ClientError>,
-    fn get_eph_rng_keypair(&self) -> Result<schnorrkel::keys::Keypair, ClientError>,
+    fn health_check(&self) -> Result<String, ClientError>,
+    fn get_public_key(&self) -> Result<secp256k1::PublicKey, ClientError>,
 );
