5d7498a6530502b999e584114bb47ba5407a1680
SFYLL <sd@seismic.systems>
2025-03-28 17:08:15 +0100

    move is sudo

--- crates/enclave-server/tests/integration/server.rs
+++ crates/enclave-server/tests/integration/server.rs
@@ -16,11 +16,26 @@ use seismic_enclave_server::key_manager::builder::KeyManagerBuilder;
 use seismic_enclave_server::key_manager::key_manager::KeyManager;
 use seismic_enclave_server::server::init_tracing;
 use seismic_enclave_server::server::EnclaveServer;
-use seismic_enclave_server::utils::test_utils::is_sudo;
 use serial_test::serial;
 use std::net::SocketAddr;
 use std::thread::sleep;
 use std::time::Duration;
+
+pub fn is_sudo() -> bool {
+    use std::process::Command;
+
+    // Run the "id -u" command to check the user ID
+    let output = Command::new("id")
+        .arg("-u")
+        .output()
+        .expect("Failed to execute id command");
+
+    // Convert the output to a string and trim any whitespace
+    let user_id = String::from_utf8(output.stdout).unwrap().trim().to_string();
+
+    // Check if the user ID is 0 (which means the user is root)
+    user_id == "0"
+}
 
 async fn test_tx_io_encrypt_decrypt(client: &EnclaveClient) {
     // make the request struct
