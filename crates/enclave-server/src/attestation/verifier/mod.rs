14ac0f28c98e6808c05a113c80f6bb933fc54182
SFYLL <sd@seismic.systems>
2025-04-01 10:49:56 +0200

    reuse ASCoreTokenClaims

--- crates/enclave-server/src/attestation/verifier/mod.rs
+++ crates/enclave-server/src/attestation/verifier/mod.rs
@@ -3,4 +3,4 @@ pub mod into_original;
 pub use verifier::{DcapAttVerifier, Data};
 
 mod as_claims;
-// pub use as_claims::{ASCoreTokenClaims, ASCustomizedClaims};
+pub use as_claims::{ASCoreTokenClaims, ASCustomizedClaims};
