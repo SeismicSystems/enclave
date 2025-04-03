71f6cb4d5df2475b6e0bfe73046069906a77a627
ssolit <sol@seismic.systems>
2025-04-03 14:52:32 +0000

    started decrypt

--- crates/enclave-server/src/server/boot/mod.rs
+++ crates/enclave-server/src/server/boot/mod.rs
@@ -7,24 +7,33 @@ use rand::rngs::OsRng;
 use rand::TryRngCore;
 use secp256k1::Secp256k1;
 use secp256k1::rand::rngs::OsRng as Secp256k1Rng;
+use seismic_enclave::{ecdh_encrypt, ecdh_decrypt, nonce::Nonce};
+use tracing::error;
+use anyhow::anyhow;
+// use zeroize::{Zeroize, ZeroizeOnDrop};
 
 pub struct Booter {
-    master_key: Option<[u8; 32]>,
+    pk: secp256k1::PublicKey,
+    sk: secp256k1::SecretKey,
+    km_master_key: Option<[u8; 32]>,
 }
 impl Booter {
     pub fn new() -> Self {
+        let secp = Secp256k1::new();
+        let (sk, pk) = secp.generate_keypair(&mut Secp256k1Rng);
         Self {
-            master_key: None,
+            pk,
+            sk,
+            km_master_key: None,
         }
     }
     /// Get the master key for the enclave server
     pub fn get_master_key(&self) -> Option<[u8; 32]> {
-        self.master_key
+        self.km_master_key
     }
 
     // assumes engine handler makes the pk and attested to it
-    pub async fn retrieve_master_key(&mut self, url: &str, retriever_pk: &secp256k1::PublicKey, retriever_sk: &secp256k1::SecretKey, attestation: &Vec<u8>) -> Result<(), anyhow::Error> {
-
+    pub async fn retrieve_master_key(&mut self, url: &str, retriever_pk: &secp256k1::PublicKey, retriever_sk: &secp256k1::SecretKey, nonce: Nonce, attestation: &Vec<u8>) -> Result<(), anyhow::Error> {
         let req = ShareMasterKeyRequest {
             retriever_pk: retriever_pk.clone(),
             attestation: attestation.clone(),
@@ -32,31 +41,24 @@ impl Booter {
 
         // TODO: probably modify seismic-enclave rpc to have a method for this
         // How will auth work? enclave x will not have enclave y's jwt
-        let res = reqwest::Client::new()
+        let http_res = reqwest::Client::new()
             .post(url)
             .json(&req)
             .send().await?;
-        let real_res: ShareMasterKeyResponse = res.json().await?;
+        let res: ShareMasterKeyResponse = http_res.json().await?;
 
         // TODO: decrypt ciphertext
-        // let master_key: [u8; 32] = decrypt(real_res.master_key_ciphertext, retriever_sk, sharer_pk)?;
-        let master_key = [0u8; 32];
+        let master_key = ecdh_decrypt(&res.sharer_pk, &retriever_sk, &res.master_key_ciphertext, nonce)?;
 
-        self.master_key = Some(master_key);
+        self.km_master_key = Some(master_key);
         Ok(())
     }
 
     // assume engine has already verified the attestation
-    pub async fn share_master_key(&mut self, sharer_pk: &secp256k1::PublicKey) -> Result<(Vec<u8>, secp256k1::PublicKey), anyhow::Error> {
-        // assume engine has already verified the attestation
-        let secp = Secp256k1::new();
-        let (sharer_sk, sharer_pk) = secp.generate_keypair(&mut Secp256k1Rng);
-        
-        // TODO:  derive encryption key, encrypt master key
-        let master_key_ciphertext = [0u8; 32];
-        let vec: Vec<u8> = master_key_ciphertext.to_vec();
-        
-        Ok((vec, sharer_pk))
+    pub async fn share_master_key(&mut self, retriever_pk: &secp256k1::PublicKey, existing_master_key: &Vec<u8>) -> Result<(Nonce, Vec<u8>, secp256k1::PublicKey), anyhow::Error> {
+        let nonce = Nonce::new_rand();
+        let master_key_ciphertext = ecdh_encrypt(&retriever_pk, &self.sk, existing_master_key, nonce.clone())?;
+        Ok((nonce, master_key_ciphertext, self.pk))
     }
 
     pub async fn genesis_boot(&mut self) -> Result<(), anyhow::Error> {
@@ -64,7 +66,7 @@ impl Booter {
         let mut rng_bytes = [0u8; 32];
         rng.try_fill_bytes(&mut rng_bytes)?;
 
-        self.master_key = Some(rng_bytes);
+        self.km_master_key = Some(rng_bytes);
         Ok(())
     }
 }
