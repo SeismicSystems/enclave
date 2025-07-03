fffa50f6bc12f071b5161ec109409b276b40ed0b
ssolit <sol@seismic.systems>
2025-07-03 14:47:01 +0000

    see_yocto_tdx_evidence() runner

--- crates/enclave-server/src/server/engine.rs
+++ crates/enclave-server/src/server/engine.rs
@@ -214,7 +214,10 @@ where
         // FUTURE WORK: make sure the "share_root" policy is up to date with on-chain votes
 
         // Verify new enclave's attestation
-        let _ = self.eval_attestation_evidence(req.clone().into()).await?;
+        let thingy: AttestationEvalEvidenceResponse = self.eval_attestation_evidence(req.clone().into()).await?;
+
+        let claims = thingy.claims.unwrap();
+        println!("claims: {:?}", claims);
 
         // Encrypt the existing root key
         let key_provider = self.key_provider()?;
@@ -467,6 +470,7 @@ mod tests {
             key_plaintext == [0u8; 32],
             "root key does not match expected mock value"
         );
+        assert!(false);
     }
 
     #[serial(attestation_agent)]
