995eaa6ee90f36aede51b8b422f9157bf892c7fb
ssolit <sol@seismic.systems>
2025-07-08 16:10:23 +0000

    fmt

--- crates/enclave/src/client/booter.rs
+++ crates/enclave/src/client/booter.rs
@@ -1,11 +1,9 @@
 //! Convenience functions for booting the enclave in a streamlined way
-use crate::EnclaveClient;
 use crate::rpc::EnclaveApiClient;
+use crate::EnclaveClient;
 
 /// Command to boot the enclave's key manager
-pub async fn boot_genesis_streamlined_async(
-    client: &EnclaveClient,
-) -> Result<(), anyhow::Error> {
+pub async fn boot_genesis_streamlined_async(client: &EnclaveClient) -> Result<(), anyhow::Error> {
     let health_check = client.health_check().await?;
     if health_check.boot_complete {
         tracing::warn!("Enclave already booted. Skipping genesis boot");
@@ -16,9 +14,7 @@ pub async fn boot_genesis_streamlined_async(
     Ok(())
 }
 
-pub fn boot_genesis_streamlined_sync(
-    client: &EnclaveClient,
-) -> Result<(), anyhow::Error> {
+pub fn boot_genesis_streamlined_sync(client: &EnclaveClient) -> Result<(), anyhow::Error> {
     let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(boot_genesis_streamlined_async(client))
 }
