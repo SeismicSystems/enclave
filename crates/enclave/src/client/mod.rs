31fba611e5877ade484535c3d5774092653b24f5
ssolit <49683577+ssolit@users.noreply.github.com>
2025-07-08 11:07:34 -0400

    Main into dev (#151)
    Co-authored-by: Christian Drappi <c@seismic.systems>

--- crates/enclave/src/client/mod.rs
+++ crates/enclave/src/client/mod.rs
@@ -4,9 +4,11 @@
 //! operations, e.g. encryption and decryption operations. The main structures and
 //! traits define the API and implementation for the TEE client.
 #![allow(async_fn_in_trait)]
+pub(crate) mod booter;
 pub mod client;
 pub mod mock;
 pub mod rpc;
 
+pub use booter::{boot_genesis_streamlined_async, boot_genesis_streamlined_sync, BootError};
 pub use client::*;
 pub use mock::*;
