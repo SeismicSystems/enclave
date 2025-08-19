31bc0926ccec1c916fd856d48aa22ff9c6eab763
cdrappi <c@seismic.systems>
2025-08-19 17:33:59 -0400

    panic

--- crates/enclave-server/build.rs
+++ crates/enclave-server/build.rs
@@ -3,6 +3,12 @@ use enclave_policies::install_all_policies;
 #[tokio::main]
 async fn main() {
     // println!("cargo:rerun-if-changed=src/utils/policy_fixture.rs");
-    install_all_policies();
-    println!("Successfully installed all policies during build");
+    match install_all_policies().await {
+        Ok(_) => {
+            println!("Successfully installed all policies during build");
+        }
+        Err(e) => {
+            panic!("Failed to install policies: {}", e);
+        }
+    }
 }
