6c5ba62b25d6ee3f1c30d4b474e94659e8677ff0
cdrappi <c@seismic.systems>
2025-08-19 15:30:04 -0400

    less pub

--- crates/enclave-server/src/server/mod.rs
+++ crates/enclave-server/src/server/mod.rs
@@ -4,4 +4,4 @@ mod into_original;
 pub mod server;
 
 // re-exports
-pub (crate) use server::{init_tracing, EnclaveServer, EnclaveServerBuilder};
+pub use server::{init_tracing, EnclaveServer, EnclaveServerBuilder};
