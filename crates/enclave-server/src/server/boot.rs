34b17f7c800dc20ca7cb3875d42a462792dc28fa
cdrappi <c@seismic.systems>
2025-10-16 15:12:27 -0400

    enclave: when booting, dont use randomness

--- crates/enclave-server/src/server/boot.rs
+++ crates/enclave-server/src/server/boot.rs
@@ -166,9 +166,8 @@ impl Booter {
     /// root key is generated using OsRng
     pub fn genesis(&self) -> Result<(), anyhow::Error> {
         // FUTURE WORK: consider using key shares instead of OsRng
-        let mut rng = OsRng;
-        let mut rng_bytes = [0u8; 32];
-        rng.try_fill_bytes(&mut rng_bytes)?;
+        // TEMPORARY: Using deterministic mock key for testing
+        let rng_bytes = [0u8; 32];
 
         let mut guard = self.km_root_key.lock().unwrap();
         *guard = Some(rng_bytes);
@@ -286,11 +285,10 @@ mod tests {
             "root key should not be empty"
         );
         let root_key = booter.get_root_key().unwrap();
-        booter.genesis().unwrap();
-        let new_root_key = booter.get_root_key().unwrap();
-        assert!(
-            root_key != new_root_key,
-            "root key genesis should be random"
+        assert_eq!(
+            root_key,
+            [0u8; 32],
+            "root key genesis should be deterministic mock key"
         );
     }
 }
